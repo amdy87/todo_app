f49ae7c7fc9245ccdd30ad347dec775d1cc46c80
austin <amoudy@wisc.edu>
2024-08-18 21:20:12 -0500

    Added drag and drop feature to task card movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { act } from "react";
 import "./App.css";
 import TaskForm from "./components/TaskForm/TaskForm";
 import TaskColumn from "./components/TaskColumn/TaskColumn";
@@ -9,6 +9,7 @@ console.log(oldTasks);
 
 const App = () => {
   const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+  const [activeCard, setActiveCard] = useState(null);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -17,6 +18,24 @@ const App = () => {
   const handleDelete = (taskIndex) => {
     const newTasks = tasks.filter((task, index) => index !== taskIndex);
     setTasks(newTasks);
+  };
+
+  const onDrop = (status, position) => {
+    console.log(
+      `${activeCard} is goint to place into ${status} and position ${position}`
+    );
+
+    if (activeCard === null || activeCard === undefined) return;
+
+    const taskToMove = tasks[activeCard];
+    const updatedTasks = tasks.filter((task, index) => index != activeCard);
+
+    updatedTasks.splice(position, 0, {
+      ...taskToMove,
+      status: status,
+    });
+
+    setTasks(updatedTasks);
   };
 
   return (
@@ -28,18 +47,24 @@ const App = () => {
           tasks={tasks}
           status="Todo"
           handleDelete={handleDelete}
+          setActiveCard={setActiveCard}
+          onDrop={onDrop}
         />
         <TaskColumn
           title="Doing"
           tasks={tasks}
           status="Doing"
           handleDelete={handleDelete}
+          setActiveCard={setActiveCard}
+          onDrop={onDrop}
         />
         <TaskColumn
           title="Done"
           tasks={tasks}
           status="Done"
           handleDelete={handleDelete}
+          setActiveCard={setActiveCard}
+          onDrop={onDrop}
         />
       </main>
     </div>
--- src/components/TaskColumn/TaskColumn.jsx
+++ src/components/TaskColumn/TaskColumn.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import "./TaskColumn.css";
 import TaskCard from "../TaskCard/TaskCard";
+import DropArea from "../Drop Area/DropArea";
 
-const TaskColumn = ({ title, tasks, status, handleDelete }) => {
+const TaskColumn = ({
+  title,
+  tasks,
+  status,
+  handleDelete,
+  setActiveCard,
+  onDrop,
+}) => {
   return (
     <section className="task_column">
       <h2 className="task_column_heading">{title}</h2>
+      <DropArea onDrop={() => onDrop(status, 0)} />
       {tasks.map(
         (task, index) =>
           task.status === status && (
-            <TaskCard
-              key={index}
-              title={task.task}
-              tags={task.tags}
-              handleDelete={handleDelete}
-              index={index}
-            />
+            <React.Fragment key={index}>
+              <TaskCard
+                title={task.task}
+                tags={task.tags}
+                handleDelete={handleDelete}
+                index={index}
+                setActiveCard={setActiveCard}
+              />
+              <DropArea onDrop={() => onDrop(status, index + 1)} />
+            </React.Fragment>
           )
       )}
     </section>
